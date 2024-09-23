ea0c3aa871fe3bfeb5933121042f6381e8752485
jlonuz <lonuz.j@gmail.com>
2024-09-22 22:30:22 -0300

    add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,12 @@ import sitemap from '@astrojs/sitemap';
 
 import node from '@astrojs/node';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: cloudflare(),
 });
